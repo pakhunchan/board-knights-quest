8951e2cf65a4c9b262fc867c121e9e1414e642fa
pakhunchan <pakhun.chan@gmail.com>
2026-04-06 11:50:04 -0500

    Add CommandLineBuild editor script for Android batch builds

---
+++ Assets/Editor/CommandLineBuild.cs
@@ -0,0 +1,29 @@
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+
+public static class CommandLineBuild
+{
+    public static void BuildAndroid()
+    {
+        var scenes = new[] { "Assets/Scenes/Nullify.unity" };
+        var options = new BuildPlayerOptions
+        {
+            scenes = scenes,
+            locationPathName = "build-file/playground.apk",
+            target = BuildTarget.Android,
+            options = BuildOptions.None
+        };
+
+        var report = BuildPipeline.BuildPlayer(options);
+        if (report.summary.result != BuildResult.Succeeded)
+        {
+            Debug.LogError($"Build failed: {report.summary.result}");
+            EditorApplication.Exit(1);
+        }
+        else
+        {
+            Debug.Log("Build succeeded!");
+        }
+    }
+}
